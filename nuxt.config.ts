9328eb52aaeaaa658d0e229ed9c6df6eaaa60579
Playneko <playneko@outlook.com>
2024-10-14 23:38:57 +0900

    ソースコード修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,40 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  ssr: false,
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  app : {
+    baseURL : '/',
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      ],
+      meta: [
+        { 'http-equiv': 'Content-Security-Policy', content: 'upgrade-insecure-requests' }
+      ]
+    }
+  },
+  css: [
+    "~/assets/css/common.css"
+  ],
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    'dayjs-nuxt',
+  ],
+  runtimeConfig: {
+    public: {
+      // API URL
+      apiList: "https://api.playneko.com/api/",
+    }
+  }
 })
